dc1a10a59610b34f34a6050331d1a3c0e774e111
Simeon <61100437+simeon2002@users.noreply.github.com>
2026-08-09 21:39:50 +0300

    Create table HTML structure, style table layout and add modal dialog to add new book

---
+++ dist/Library.672d4772.js
@@ -0,0 +1,11 @@
+"use strict";
+const btnAddNewBook = document.querySelector(".btn--add");
+const bookModal = document.querySelector("dialog");
+const btnSubmitModal = document.querySelector(".btn--modal");
+bookModal.showModal();
+btnAddNewBook.addEventListener("click", (e)=>{
+    bookModal.showModal();
+});
+btnSubmitModal.addEventListener("click", (e)=>{});
+
+//# sourceMappingURL=Library.672d4772.js.map
--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const btnAddNewBook = document.querySelector(".btn--add");
+const bookModal = document.querySelector("dialog");
+const btnSubmitModal = document.querySelector(".btn--modal");
+
+bookModal.showModal();
+btnAddNewBook.addEventListener("click", e => {
+  bookModal.showModal();
+});
+
+btnSubmitModal.addEventListener("click", e => {});
